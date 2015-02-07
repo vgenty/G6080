d0766a60b8d795f77ce9d856b1b639646cb42abe
Victor Genty <vic.genty@gmail.com>
2015-02-06 23:39:16 -0500

    played around it problem 1 trying to truncate via floor

--- ps1/problem1/Recursion.cxx
+++ ps1/problem1/Recursion.cxx
@@ -2,6 +2,7 @@
 
 Recursion::Recursion() {
      c_ = cos(x_);
+     precision_ = 12;
 }
 
 Recursion::~Recursion() {}
@@ -16,6 +17,41 @@ double Recursion::value(const double& N) {
   for( double  _k = 1; _k <  N-1; ++_k)
     _cc->at(_k+1) = 2.0 * c_ * _cc->at(_k) - _cc->at(_k-1);
   
+  return _cc->at(N-1);
+  
+}
+
+double Recursion::isvalue(const double& N) {
+  std::vector<double> *_cc = new std::vector<double>();
+  _cc->resize(N,0);
+  
+  //Reduce precision for initial guess say 0.2
+  _cc->at(0) = c_ - 0.2;
+  _cc->at(1) = 2.0 * c_ * c_ - 1.0;
+  
+  for( double  _k = 1; _k <  N-1; ++_k)
+    _cc->at(_k+1) = 2.0 * c_ * _cc->at(_k) - _cc->at(_k-1);
+  
+  return _cc->at(N-1);
+  
+}
+
+double Recursion::rsvalue(const double& N) {
+std::vector<double> *_cc = new std::vector<double>();
+  _cc->resize(N,0);
+  
+  //Keep the initial guess kosher
+  _cc->at(0) = c_;
+  
+  //Round 
+  _cc->at(1) = roundd(2.0 * c_ * c_ - 1.0);
+  std::cout << std::setprecision(15) << _cc->at(1) << std::endl;
+  std::cout << std::setprecision(15) << 2.0 * c_ * c_ - 1.0 << std::endl;
+  
+  for( double  _k = 1; _k <  N-1; ++_k) {
+    _cc->at(_k+1) = roundd(roundd(2.0 * c_ * _cc->at(_k)) - roundd(_cc->at(_k-1)));
+    //    std::cout << std::setprecision(15) << _cc->at(_k+1) << " ";
+  }
   return _cc->at(N-1);
   
 }
--- ps1/problem1/Recursion.h
+++ ps1/problem1/Recursion.h
@@ -13,10 +13,17 @@ public:
   virtual ~Recursion();
   
   double value(const double& N);
+  double isvalue(const double& N);
+  double rsvalue(const double& N);
   
 private:
   const double x_ = pow(10,-6); // Initial angle value
   double c_;       // Stores the value of cosine
+  int precision_;
+  
+  double roundd(double num) {
+    return floor(num * pow(10.0,precision_))/pow(10.0,precision_);
+  }
   
 protected:
 
