6a88258bffe97eceb0578c55f7edebbe87ebcc47
Victor Genty <vic.genty@gmail.com>
2015-02-20 14:31:25 -0500

    I think I got it working...

--- ps2/planets/evolve.py
+++ ps2/planets/evolve.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python
 import numpy as np
-
+from ROOT import TCanvas, TAxis, TGraph, TMultiGraph
 import planet
 
 def main():
-    nsteps = 30000
-    NP  = 3                # num planets
-
-    p = planet.Planet(nsteps,NP)
-
-    p.evolve()
+    nsteps = 500
     
+    NP  = 3                # num planets
+    
+    p = planet.Planet(nsteps,NP)
+    
+    p.evolve()
+    tg1 = TGraph()
+    tg2 = TGraph()
+    tmg = TMultiGraph()
+    
+    tmg.Add(tg1)
+    tmg.Add(tg2)
+    
+    for i in xrange(len(p.r)):
+        tg1.SetPoint(i,p.r[i][0][0],p.r[i][0][1])
+        tg2.SetPoint(i,p.r[i][1][0],p.r[i][1][1])
+ 
+    tg1.SetLineColor(3)
+    tg2.SetLineColor(2)
+    tmg.Draw("AL")
+    raw_input('')
+
+    # print "==r=="
+    # print p.r
+    # print "==v=="
+    # print p.v
+    # print "==fr=="
+    # print p.fr
+    # print "==fv=="
+    # print p.fv
 if __name__ == '__main__':
     main()
--- ps2/planets/planet.py
+++ ps2/planets/planet.py
@@ -6,6 +6,8 @@ class Planet:
         self.nsteps = nsteps
         self.NP     = NP
 
+        self.dx = pow(2,-4.0)
+    
         self.r  = np.zeros((nsteps,NP,3),dtype=float)    # initial position
         self.v  = np.zeros((nsteps,NP,3),dtype=float)    # initial velocity
         
@@ -15,7 +17,7 @@ class Planet:
         
         self.ke = np.zeros((NP,nsteps),dtype=float)
         self.pe = np.zeros((NP,nsteps),dtype=float)
-            
+        
         self.KEtot = np.zeros(nsteps,dtype=float)
         self.PEtot = np.zeros(nsteps,dtype=float)
         self.Etot  = np.zeros(nsteps,dtype=float)
@@ -32,10 +34,9 @@ class Planet:
         self.v[0][1][1] =  1.00
         self.v[0][2][1] =  1.04        
 
-        self.F(0)
-                
-        print self.fv
-
+        for k in xrange(3):
+            self.F(0,k)
+            
         self.ff = 0.0;
         self.denom = 0.0;
 
@@ -47,24 +48,53 @@ class Planet:
         return float(0.5 * self.m * ( self.v[0]**2 + 
                                       self.v[1]**2 + 
                                       self.v[2]**2 ) )            
+        
+    def evolve(self):
+        for i in xrange(self.nsteps-1):
+            for j in xrange(self.NP):
+                #will clean up
+                for k in xrange(3):
+                    self.r[i+1][j][k] = self.r[i][j][k] + self.dx * self.fr[i][j][k]
+                    self.v[i+1][j][k] = self.v[i][j][k] + self.dx * self.fv[i][j][k]
 
-    def F(self,i):
-        for j in xrange(self.NP): #planet number
-            for k in xrange(3):   # x,y,z
-                self.fr[i][j][k] = self.v[i][j][k] 
-                self.fv[i][j][k] = self.force(i,j,k) #calculate force on j'th planet along k direction
+            for j in xrange(self.NP):
+                for k in xrange(3):
+                    self.F(i+1,k)
+
+            for j in xrange(self.NP):
+                for k in xrange(3):
+                    self.r[i+1][j][k] = self.r[i][j][k] + self.dx/2.0 * (self.fr[i][j][k]
+                                                                         + self.fr[i+1][j][k])
+                    self.v[i+1][j][k] = self.v[i][j][k] + self.dx/2.0 * (self.fv[i][j][k]
+                                                                         + self.fv[i+1][j][k])
+
+            for j in xrange(self.NP):
+                for k in xrange(3):
+                    self.F(i+1,k)
+
+
+    def F(self,i,k):
+        for j in xrange(self.NP): # planet number
+            #for k in xrange(3):   # x,y,z
+            self.fr[i][j][k] = self.v[i][j][k] 
+            self.fv[i][j][k] = self.force(i,j,k) #calculate force on j'th planet along k direction
         
     def force(self,i,j,k):
-        self.ff = 0.0             # force on planet j
+        #print "@@@@@@@@@@@@@@@@ i = %d @@@@@@@@@@@@@" % i
+        #print self.r[i][j]
+        self.ff = 0.0            # force on planet j
         for l in xrange(self.NP): # planet number l
-            self.denom = 0.0;
+            self.denom = 0.0
             if j is not l:
                 for b in xrange(3):
-                    self.denom += (self.r[i][l][b] - self.r[i][j][b])**2
+                    #print "Planet %d at %0.15f on %d %0.15f, coordinate %d" % (j,self.r[i][j][b],l,self.r[i][l][b],b)
+                    #print "self.denom = %0.15f" % self.denom
+                    self.denom += pow(self.r[i][l][b] - self.r[i][j][b],2.0)
+                    
+                #print "self.ff = %0.15f" % self.ff
+                self.ff += (self.m[l] * (self.r[i][j][k] - self.r[i][l][k]) / pow(self.denom,1.5))
+                #print "diff: %0.15f: %f - %f " % (self.r[i][j][k],self.r[i][l][k],(self.r[i][j][k] - self.r[i][l][k]))
+                #print "self.ff = %0.15f" % self.ff
 
-                self.ff += (self.m[l] * \
-                            (self.r[i][j][k] - self.r[i][l][k]) \
-                            /\
-                            self.denom**(1.5))
-        
+
         return -1.0*self.ff
